977f986cdbb30a1f0730a932a12a983e978b2451
James Lins <jlalencar@gmail.com>
2022-01-09 13:32:50 -0300

    Aula

---
+++ Curso/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool completo = false;
+char genero = 'F';
+char letra = '\u0041';
+byte n1 = 126;
+int n2 = 1000;
+int n3 = 2147483647;
+long n4 = 2147483648L;
+float n5 = 4.5f;
+double n6 = 4.5;
+string nome = "Maria";
+int n7 = int.MinValue;
+int n8 = int.MaxValue;
+sbyte n9 = sbyte.MinValue;
+decimal n10 = decimal.MaxValue;
+
+Console.WriteLine(completo);
+Console.WriteLine(genero);
+Console.WriteLine(letra);
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+Console.WriteLine(n4);
+Console.WriteLine(n5);
+Console.WriteLine(n6);
+Console.WriteLine(nome);
+Console.WriteLine(n7);
+Console.WriteLine(n8);
+Console.WriteLine(n9);
+Console.WriteLine(n10);
